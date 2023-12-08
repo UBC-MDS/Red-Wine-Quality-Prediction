1be373ad016ed98dc8deb342279f4662c81c78c3
sungg888 <42681419+sungg888@users.noreply.github.com>
2023-12-08 01:33:56 -0800

    Update data_split_test.py

--- tests/data_split_test.py
+++ tests/data_split_test.py
@@ -1,54 +1,78 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 
 
-import pandas as pd
-import pytest
 import sys
 import os
+import pytest
+import click.testing
+import pandas as pd
 
-sys.path.append('..')
-from scripts.data_split import splitfunction
+# Adjust the import path to include your script
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from scripts.data_split import main  # Replace with the actual directory name where your script is located
 
-from sklearn.model_selection import (
-    GridSearchCV,
-    RandomizedSearchCV,
-    cross_validate,
-    train_test_split,
-)
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
 
-import numpy as np
+# Test if the function executes without errors for valid inputs
+def test_function_execution(runner):
+    with runner.isolated_filesystem():
+        # Create a dummy dataset with a semicolon delimiter
+        df = pd.DataFrame({'feature1': range(10), 'quality': range(10)})
+        df.to_csv('test_data.csv', index=False, sep=';')
 
+        # Create the required directory structure
+        os.makedirs('results/tables', exist_ok=True)
 
-# In[2]:
+        result = runner.invoke(main, ['test_data.csv', 'x_train.csv', 'x_test.csv', 
+                                      'y_train.csv', 'y_test.csv', '0.2', '42'])
 
+        assert result.exit_code == 0
 
-#Test data
-np.random.seed(522)
+# Test for output file creation
+def test_output_file_creation(runner):
+    with runner.isolated_filesystem():
+        df = pd.DataFrame({'feature1': range(10), 'quality': range(10)})
+        df.to_csv('test_data.csv', index=False, sep=';')
 
-# Generating random data for X and y
-X = np.random.rand(100, 10) # 100 samples with 10 features each
-y = np.random.randint(0, 2, 100) # 100 samples with binary labels (0 or 1)
+        # Create the required directory structure
+        os.makedirs('results/tables', exist_ok=True)
 
+        runner.invoke(main, ['test_data.csv', 'x_train.csv', 'x_test.csv', 
+                             'y_train.csv', 'y_test.csv', '0.2', '42'])
 
-#Expected outputs from directly using sklearn functions
+        # Check for files in the expected directory
+        assert os.path.exists('results/tables/x_train.csv')
+        assert os.path.exists('results/tables/x_test.csv')
+        assert os.path.exists('results/tables/y_train.csv')
+        assert os.path.exists('results/tables/y_test.csv')
 
+# Test for invalid input file
+def test_invalid_input_file(runner):
+    with runner.isolated_filesystem():
+        # Create the required directory structure
+        os.makedirs('results/tables', exist_ok=True)
 
-# Test Cases
-def test_basic_functionality():
-    X_train, X_test, y_train, y_test = splitfunction(X, y, 0.25, 42)
-    assert len(X_train) == 75
-    assert len(X_test) == 25
-    assert len(y_train) == 75
-    assert len(y_test) == 25
+        # Invoke main with a non-existent file
+        result = runner.invoke(main, ['non_existent_file.csv', 'x_train.csv', 'x_test.csv', 
+                                      'y_train.csv', 'y_test.csv', '0.2', '42'])
 
-def test_test_size_proportion():
-    _, X_test, _, _ = splitfunction(X, y, 0.30, 42)
-    assert len(X_test) == 30
+        assert result.exit_code != 0
+       
 
-def test_random_state_consistency():
-    _, X_test1, _, _ = splitfunction(X, y, 0.25, 42)
-    _, X_test2, _, _ = splitfunction(X, y, 0.25, 42)
-    assert np.array_equal(X_test1, X_test2)
+# Test for incorrect test_size or random_state
+def test_invalid_test_size_or_random_state(runner):
+    with runner.isolated_filesystem():
+        # Create a dummy dataset
+        df = pd.DataFrame({'feature1': range(10), 'quality': range(10)})
+        df.to_csv('test_data.csv', index=False, sep=';')
+
+        # Create the required directory structure
+        os.makedirs('results/tables', exist_ok=True)
+
+        # Invoke main with invalid test_size and random_state
+        result = runner.invoke(main, ['test_data.csv', 'x_train.csv', 'x_test.csv', 
+                                      'y_train.csv', 'y_test.csv', '1.5', '-1'])
+
+        assert result.exit_code != 0
+       
